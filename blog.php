acfa2353a018567cb27952a3578f0b5b368ce39b
katilla <atilla.karman@port.hu>
2015-12-14 15:32:28 +0100

    initial commit

---
+++ blog.php
@@ -0,0 +1,17 @@
+<?php
+	echo $_POST ["textarea"];
+?>
+
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<form action="blog.php" method="post">
+<textarea id="textarea" name="textarea" rows="10" cols="50">
+</textarea>
+<br />
+<input type="submit" value="Előnézet">
+</form>
+</body>
+</html>
