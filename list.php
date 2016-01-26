58b03983cb80f7271968a76aabb7e6c14f813803
Wonkowich <lajos.papp.ifj@gmail.com>
2016-01-26 11:58:58 +0100

    fetchAll módszerrel nyeri ki most az adatokat.

--- list.php
+++ list.php
@@ -3,11 +3,9 @@ $connect = new PDO ('mysql:host=localhost;dbname=blog','root','');
 
 $query = $connect->query('SELECT * FROM post');
 
-while($r = $query->fetch()) {
-	
-	echo $r['post'];
-	echo $r['id'], '<br />';
+$query->execute();
 
-}
+$result = $query->fetchAll();
+print_r($result);
 
 ?>
