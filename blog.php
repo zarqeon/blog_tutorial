9daa6dc479eb9e97c3f3fbfc950284a4c4184b4c
Wonkowich <lajos.papp.ifj@gmail.com>
2016-02-11 16:03:17 +0100

    $single_tag trimmelve van.

--- blog.php
+++ blog.php
@@ -1,8 +1,8 @@
 <?php
 
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+	//ini_set('display_errors', 1);
+	//ini_set('display_startup_errors', 1);
+	//error_reporting(E_ALL);
 	
 	echo $_POST ["textarea"];
 	$textarea = ($_POST["textarea"]);
@@ -88,8 +88,8 @@
 		
 		foreach($exploded_tags as $single_tag){
 		
-		$dot .= '"'. $single_tag . '",';
-			
+		$dot .= '"'. trim($single_tag) . '",';
+		
 		}
 		
 		$trimmed = rtrim ($dot, ",");
