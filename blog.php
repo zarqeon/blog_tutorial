85d55c58e8879577ff29a494de7661a55fe45d93
Wonkowich <lajos.papp.ifj@gmail.com>
2016-02-19 12:25:59 +0100

    Leírtam mit csinálnak a sorok. Arra gondoltam hogy minden nagyobb rész elé még leírom majd, hogy mit csinálnak úgy nagyobb értelemben. És az magyarázza meg az alkalmazáslogikát, nemtudom így gondoltad e.

--- blog.php
+++ blog.php
@@ -4,109 +4,108 @@
 	//ini_set('display_startup_errors', 1);
 	//error_reporting(E_ALL);
 	
-	echo $_POST ["textarea"];
-	$textarea = ($_POST["textarea"]);
-	echo $_POST ["tags"];
-	$tags = ($_POST["tags"]);
-	$id_post = ($_REQUEST['id']);
+	echo $_POST ["textarea"];	// Kiechozza mi lett POST módszerrel elküldve a textareából
+	$textarea = ($_POST["textarea"]);	// Deklarálja a $textarea változót, ami textarea mezőből postolt értékekkel egyenlő
+	echo $_POST ["tags"];	// Kiechózza mi lett POST módszerrel elküldve a tags input mezőből
+	$tags = ($_POST["tags"]);	// Deklarálja a $tags változót, ami a tags inputmezőből postolt értékekkel egyenlő
+	$id_post = ($_REQUEST['id']);	// Deklarálja az $id_post változót (az URL-ben lett átküldve a list.php fájlból, és REQUEST módszerrel lett beszerezve)
 	
 	
-	function validate($variable)
+	function validate($variable)	//deklarálja a validate nevű függvényt, és egy változót ad meg paraméternek
 	{
-		if(!empty($variable))
+		if(!empty($variable))	//A feltétel teljesül, ha a megadott változónak van értéke.
 		{
-			return true;
+			return true;	//ha teljesült a feltétel, "true" értéket ad vissza
 		}
 
-		return false;
+		return false;	//ha nem teljesült a feltétel, "false" értéket ad vissza
 	}
 
-	function validate_button ($textarea, $tags)
+	function validate_button ($textarea, $tags)	//deklarálja a validate_button nevű függvényt, és a $textarea, és a $tags változókat adja meg paraméternek
 	{
-		if(validate($textarea) && validate($tags))
+		if(validate($textarea) && validate($tags))	//A feltétel akkor teljesül, ha a validate függvény lefutása a $textarea ÉS a $tags változók felhasználásval true értéked ad vissza
 		{
-			echo '<input type="submit" name="Közzétesz" value="Közzétesz">';		
+			echo '<input type="submit" name="Közzétesz" value="Közzétesz">';	//ha teljesült a feltétel, akkor egy közzétesz buttont echóz a függvény
 		}	
 	}
 	
-	$connect = new PDO ('mysql:host=localhost;dbname=blog','root','');
+	$connect = new PDO ('mysql:host=localhost;dbname=blog','root','');	//Deklarálja a $connect változót. Ez egy PDO segítségével kapcsolatot hoz létre az adatbázissal.
 
 	
 	
-	if(!empty($textarea))
+	if(!empty($textarea)) //Az elágazás feltétele az, hogy a $textarea változó ne legyen üres
 	{
 		
-		$repost=$textarea;
+		$repost=$textarea;	//Ha teljesült a feltétel, a $repost változót egyenlővé teszi a $textarea változóval
 		
-	}	else if(!empty($id_post)) {
-		$edit_post=$connect->prepare("SELECT post FROM post WHERE id=?");
-		$data=array($id_post);
-		$edit_post->execute($data);
-		$result=$edit_post->fetchColumn();
-		$repost=$result;
+	}	else if(!empty($id_post)) {		//Ennek az elágazásnak a feltétele, hogy az $id_post változó ne legyen üres
+		$edit_post=$connect->prepare("SELECT post FROM post WHERE id=?");	//Ha teljesült a feltétel, ez a sor létrehoz egy statementet, ami kiválaszt egy mezőt a post táblából egy id alapján
+		$data=array($id_post);	//deklarálja a $data változót, ami egyenértékű az $id_posttal
+		$edit_post->execute($data);		//végrehajtja az edit_post statementet, a $data változót használva paraméterként
+		$result=$edit_post->fetchColumn();		//fetcheli azt az oszlopot amit az edit_post statement kapott eredményként, és egyenlővé teszi az eredményt az $result változóval
+		$repost=$result;	//deklarálja a $repost változót, ami a $result változóval egyenértékű
 		
 	}
 
 
 	
-	function hidden_input ($id_post)
+	function hidden_input ($id_post)	//deklarálja a hidden_input nevű függvényt, az $id_post változót felhasználva paraméterként
 	{
-		if (!empty ($id_post))
+		if (!empty ($id_post))	//az elágazás feltétele az, hogy az $id_post változó ne legyen üres
 			{
-				echo "<input type='hidden' name='id' value='$id_post'>";
+				echo "<input type='hidden' name='id' value='$id_post'>";	//ha teljesült a feltétel, ezt a sort echózza ki, egy láthatatlan input mezőt
 			}
 	}
 	
 
 
 	
-	if (isset($_POST['Közzétesz']) && (validate($textarea) && validate($tags))) 
+	if (isset($_POST['Közzétesz']) && (validate($textarea) && validate($tags))) //ennek az elágazásnak az a feltétele, hogy le legyen nyomva a közzétesz gomb, ÉS fusson le a validate függvény a $textarea-ra, és a $tags-ra
 	{	
-		if(!empty($id_post)){
-		$stmnt = $connect->prepare("UPDATE post SET post =? WHERE id=?");
-		$stmnt->execute(array($textarea, $id_post));
+		if(!empty($id_post)){	//az elágazás feltétele, hogy ne legyen üres az $id_post változó
+		$stmnt = $connect->prepare("UPDATE post SET post =? WHERE id=?");	//létrehoz egy statementet, ami frissíti a post táblának post oszlopát, abban a sorban, amelyben a megadott id szerepel.
+		$stmnt->execute(array($textarea, $id_post));	//végrehajtja a statementet, a $textarea, és az $id_post változókat felhasználva
 		}
 		
-		else{
+		else{	//ha nem teljesült a fenti feltétel, az alábbi kódot hajtja végre
 			
-			$statement = $connect->prepare("INSERT INTO post(post)VALUES(?)");
-			$statement->execute(array($textarea));
-			$post_id = $connect->lastInsertId();
+			$statement = $connect->prepare("INSERT INTO post(post)VALUES(?)");	//létrezhoz egy statementet, ami beszúr egy értéket a post tábla post oszlopába
+			$statement->execute(array($textarea));	//végrehajtja a statementet, a $textarea változót felhasználva paraméterként (ennek a változónak az értékét szúrja majd be)
+			$post_id = $connect->lastInsertId();	//egyenlővé teszi a $post_id változót a legutóbb beszúrt id értékével.
 		}
 	}	
 	
 	
 	
 	
-	
-	function processTags ($connect, $tags, $post_id){
+	function processTags ($connect, $tags, $post_id){ //deklarálja a porcessTags függvényt, és a $connect, $tags, és $post_id változókat használja paraméterként
 		
-		$exploded_tags = explode(",", $tags);
+		$exploded_tags = explode(",", $tags);	//explode-olja a $tags stringet, a vesszőknél fogva
 		
-		foreach($exploded_tags as $single_tag){
-			$single_tag = trim($single_tag);
+		foreach($exploded_tags as $single_tag){	//létrehoz egy foreach loopot, amiben az exploded_tags elemeiből $single_tag változót csinál
+			$single_tag = trim($single_tag);	//leveszi a whitespace-t a $single_tag stringek elejéről és végéről
 			
-			$query = $connect->prepare("SELECT ID FROM tag WHERE tag = '$single_tag'");
-			$query->execute(array($single_tag));
-			$q_result = $query->fetchColumn();
-			$all_tagid = $q_result;
+			$query = $connect->prepare("SELECT ID FROM tag WHERE tag = '$single_tag'");	//létrehoz egy select statementet, ami kiválaszt egy id-t a tag tábla tag oszlopából, ahol a tag a $single_tag változóval azonos
+			$query->execute(array($single_tag));	//végrehajta a statementet, a $single_tag változót felhasználva paraméterként
+			$q_result = $query->fetchColumn();	//deklarálja a $q_result változót, ami egyenértékű a $query statement eredményének egy oszlopával.
+			$all_tagid = $q_result;	//deklarálja az $all_tagid változót, ami a $q_result változóval egyenértékű
 
-		if(empty($q_result)){
-			$statement = $connect->prepare("INSERT INTO tag(tag)VALUES(?)");
-			$statement->execute(array($single_tag));
-			$last_id = $connect->lastInsertId();
-			$all_tagid = $last_id;
+		if(empty($q_result)){	//az elágazás feltétele akkor teljesül, ha a $q_result változó üres
+			$statement = $connect->prepare("INSERT INTO tag(tag)VALUES(?)");	//létrehoz egy statementet, ami beszúr egy értéket a tag tábla tag oszlopába
+			$statement->execute(array($single_tag));	//végrehajtja a statementet a $single_tag változót felhasználva paraméterként
+			$last_id = $connect->lastInsertId();	//deklarálja a $last_id változót, ami az utoljára beszúrt id értékével egyenlő
+			$all_tagid = $last_id;	//deklarálja az $all_tagid változót, ami a $last_id-vel egyenértékű
 		
 		}
-			$statement = $connect->prepare("INSERT INTO posttotag(tag_id, post_id) VALUES(?,?)");
-			$statement->execute(array($all_tagid, $post_id));
+			$statement = $connect->prepare("INSERT INTO posttotag(tag_id, post_id) VALUES(?,?)");	//létrehoz egy statementet, ami beszúr a posttotag tábla két oszlopába két értéket
+			$statement->execute(array($all_tagid, $post_id));	//végrehatja a statementet, felhasnzálva az $all_tagid, és a $post_id változókat paraméterként
 		}
 		
 	}
 
-	if(isset($_POST['Közzétesz'])){
+	if(isset($_POST['Közzétesz'])){	//az elágazást ahhoz a feltételhez köti, hogy a közzétesz gomb le legyen nyomva
 	
-		processTags($connect, $tags, $post_id);
+		processTags($connect, $tags, $post_id);	//meghívja a processTags függvényt, a $connect, $tags, és a $post_id változókat használva paraméterként
 
 	}
 
@@ -121,7 +120,7 @@
 <body>
 <form action="blog.php" method="post">
 <textarea id="textarea" name="textarea" rows="10" placeholder="Ide írj" cols="50">
-<?php echo $repost?>
+<?php echo $repost //kiechózza a $repost változó értékét, így marad meg a textarea tartalma ?>
 </textarea>
 <br />
 <input type="text" id="tags" name="tags" value = "<?php echo $tags ?>">
@@ -129,8 +128,8 @@
 <input type="submit" name="Előnézet"value="Előnézet">
 <br />
 <?php
-	validate_button($textarea, $tags); 
-	hidden_input($id_post);
+	validate_button($textarea, $tags); //meghívja a validate_button függvényt, a $textarea, és a $tags változókat felhasználva paraméterként
+	hidden_input($id_post); //meghívja a hidden_input függvényt, az $id_post változót felhasználva paraméterként
 ?>
 
 </form>
