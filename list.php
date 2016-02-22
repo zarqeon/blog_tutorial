beb9a0293abe98e32268d163768de7ad82a41b93
Wonkowich <lajos.papp.ifj@gmail.com>
2016-02-23 00:01:25 +0100

    Most kicsit ezek a statementek egymásra épülnek, és az a baj, hogy nem tudom hogy használjam fel a $result['id']-t ehhez. Mert ebből kellene elindulni.

--- list.php
+++ list.php
@@ -7,13 +7,28 @@ error_reporting(E_ALL);
 $connect = new PDO ('mysql:host=localhost;dbname=blog','root','');
 $query = $connect->query('SELECT * FROM post');
 $result = $query->fetchAll();
-
-$count_tag = $connect->query('SELECT COUNT (*) FROM posttotag');
-$number = $count_tag->fetchAll();
         
 foreach($result as $id ) {
+    
+    //ez a statemenet számolná meg, hogy hány tag kapcsolódik az adott posthoz
+    $count_tag = $connect->query('SELECT COUNT(post_id) FROM posttotag WHERE post_id=?');
+    $number = $count_tag->fetchAll();//valahogy el kéne íréni hogy "$result['id']" -re hajtsa végre
+
+    //ez a statement választaná ki, a post által használt tagok id-jét, ha a post_id egyenlő lenne a $numberral
+    $list_tag = $connect->query('SELECT tag_id FROM posttotag WHERE post_id=?');
+    $listed = $list_tag->fetchAll();
+    
+    //ez a statement adná meg az id- alapján a tagokat magukat amik a posthoz kapcsolódnak, ha az id egyenlő lenne a $listed értékével
+    $echo_tags = $connect->query('SELECT tag FROM tag WHERE id=?');
+    $relisted = $echo_tags->fetchAll();
+
+    
+    
+    
     echo '<a href="blog.php?id='.$id['id'].'">', $id['id'], '</a>';
-    echo $id['post'], '<br />';  
+    echo $id['post'];  
+    echo '(',$number, ')';
+    echo $relisted, '<br />';
 }
 
 
