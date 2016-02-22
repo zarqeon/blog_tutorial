3c07aaa8a0eab1c6b6c157e3faed302045f0fc53
Wonkowich <lajos.papp.ifj@gmail.com>
2016-02-22 17:05:21 +0100

    FetchAll hibaüzenet (non object)

--- list.php
+++ list.php
@@ -5,15 +5,15 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $connect = new PDO ('mysql:host=localhost;dbname=blog','root','');
-
 $query = $connect->query('SELECT * FROM post');
-
 $result = $query->fetchAll();
 
-
+$count_tag = $connect->query('SELECT COUNT (*) FROM posttotag');
+$number = $count_tag->fetchAll();
+        
 foreach($result as $id ) {
-    echo $id['post'];
-    echo '<a href="blog.php?id='.$id['id'].'">', $id['id'], '<br />', '</a>';
+    echo '<a href="blog.php?id='.$id['id'].'">', $id['id'], '</a>';
+    echo $id['post'], '<br />';  
 }
 
 
