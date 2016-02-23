9e278caffc93a8f4afc464f386af4e36bb6afedc
Wonkowich <lajos.papp.ifj@gmail.com>
2016-02-23 13:30:58 +0100

    Array to string conversion hibaüzenet

--- list.php
+++ list.php
@@ -11,24 +11,18 @@ $result = $query->fetchAll();
 foreach($result as $id ) {
     
     //ez a statemenet számolná meg, hogy hány tag kapcsolódik az adott posthoz
-    $count_tag = $connect->query('SELECT COUNT(post_id) FROM posttotag WHERE post_id=?');
+    $count_tag = $connect->query('SELECT COUNT(post_id) FROM posttotag WHERE post_id=98');
     $number = $count_tag->fetchAll();//valahogy el kéne íréni hogy "$result['id']" -re hajtsa végre
 
-    //ez a statement választaná ki, a post által használt tagok id-jét, ha a post_id egyenlő lenne a $numberral
-    $list_tag = $connect->query('SELECT tag_id FROM posttotag WHERE post_id=?');
-    $listed = $list_tag->fetchAll();
-    
-    //ez a statement adná meg az id- alapján a tagokat magukat amik a posthoz kapcsolódnak, ha az id egyenlő lenne a $listed értékével
-    $echo_tags = $connect->query('SELECT tag FROM tag WHERE id=?');
-    $relisted = $echo_tags->fetchAll();
+    $echo_tags = $connect->query('SELECT tag.tag, posttotag.tag_id FROM tag INNER JOIN posttotag ON tag.ID=posttotag.tag_id WHERE post_id=98');
+    $tag_list = $echo_tags->fetchAll();
 
-    
-    
+    var_dump($number);
     
     echo '<a href="blog.php?id='.$id['id'].'">', $id['id'], '</a>';
     echo $id['post'];  
     echo '(',$number, ')';
-    echo $relisted, '<br />';
+    echo $tag_list, '<br />';
 }
 
 
