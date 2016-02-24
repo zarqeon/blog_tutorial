0bd72a41ee5d26929effbb22a942c28338886d3c
Wonkowich <lajos.papp.ifj@gmail.com>
2016-02-24 14:29:53 +0100

    Paraméterezett statementek

--- list.php
+++ list.php
@@ -9,20 +9,28 @@ $query = $connect->query('SELECT * FROM post');
 $result = $query->fetchAll();
         
 foreach($result as $id ) {
-    
+
     //ez a statemenet számolná meg, hogy hány tag kapcsolódik az adott posthoz
-    $count_tag = $connect->query('SELECT COUNT(post_id) FROM posttotag WHERE post_id=99');
-    $count_tag->execute(array());
+    $count_tag = $connect->prepare('SELECT COUNT(post_id) FROM posttotag WHERE post_id=?');
+    $count_tag->execute(array($id['id']));
     $number = $count_tag->fetchColumn();//valahogy el kéne íréni hogy "$result['id']" -re hajtsa végre
 
     
-    $echo_tags = $connect->query('SELECT tag.tag, posttotag.tag_id FROM tag INNER JOIN posttotag ON tag.ID=posttotag.tag_id WHERE post_id=98');
+    $echo_tags = $connect->prepare('SELECT tag.tag, posttotag.tag_id FROM tag INNER JOIN posttotag ON tag.ID=posttotag.tag_id WHERE post_id=?');
+    $echo_tags->execute(array($id['id']));
     $tag_list = $echo_tags->fetchAll(); 
+    
     
     
     echo '<a href="blog.php?id='.$id['id'].'">', $id['id'], '</a>';
     echo $id['post'];  
-    echo $number;    
+    echo '(', $number, ')';    
+    
+    foreach($tag_list as $name)
+    {   
+        echo $name['tag'], ', ';
+    }
+    
     echo '<br />';   
 }
 
