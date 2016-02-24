d879c5debcbcf3b05412c6f4e90110dbbe170ac1
Wonkowich <lajos.papp.ifj@gmail.com>
2016-02-24 13:52:04 +0100

    $number echózása

--- list.php
+++ list.php
@@ -11,25 +11,18 @@ $result = $query->fetchAll();
 foreach($result as $id ) {
     
     //ez a statemenet számolná meg, hogy hány tag kapcsolódik az adott posthoz
-    $count_tag = $connect->query('SELECT COUNT(post_id) FROM posttotag WHERE post_id=98');
-    $number = $count_tag->fetchAll();//valahogy el kéne íréni hogy "$result['id']" -re hajtsa végre
+    $count_tag = $connect->query('SELECT COUNT(post_id) FROM posttotag WHERE post_id=99');
+    $count_tag->execute(array());
+    $number = $count_tag->fetchColumn();//valahogy el kéne íréni hogy "$result['id']" -re hajtsa végre
 
+    
     $echo_tags = $connect->query('SELECT tag.tag, posttotag.tag_id FROM tag INNER JOIN posttotag ON tag.ID=posttotag.tag_id WHERE post_id=98');
     $tag_list = $echo_tags->fetchAll(); 
     
+    
     echo '<a href="blog.php?id='.$id['id'].'">', $id['id'], '</a>';
     echo $id['post'];  
-        //minden $number tömbnek csak a nulladik elemét jeleníti meg
-        foreach($number as $numtags){
-            echo '(',$numtags['0'],')';
-        }
-    
-        //minden $tag_list tömbnek csak a 'tag' részét jeleníti meg
-        foreach($tag_list as $name)
-        {   
-            echo $name['tag'], ', ';
-        }
-        
+    echo $number;    
     echo '<br />';   
 }
 
