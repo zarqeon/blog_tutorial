4716c7763afb3025c76db47c5245ded8a06ed7c9
Wonkowich <lajos.papp.ifj@gmail.com>
2016-08-19 11:00:14 +0100

    Most csináltam egy függvényt, és egyelőre átraktam abba a kódot. A listában azt hiszem jól használtam ezt a statikus hívást. Ezzel a statikus dologgal viszont nem lehet használni a $this dolgot. Gondolom ez befolyásolja hogy kéne csinálni a dolgot hogy objektumorientált legyen ahogy akarjuk. Attól hogy belevágtam ezt a kódot még nem lesz olyan nyilván.

--- Post.php
+++ Post.php
@@ -149,6 +149,41 @@ class Post extends Model{
 		
 		$this->PosttoTag();
 	}
+	
+	public static function PostList ()
+	{	
+		$connect = new PDO ('mysql:host=localhost;dbname=blog','root','4fhc9imz');
+		$query = $connect->query('SELECT * FROM post');
+		$result = $query->fetchAll();
+		
+		foreach($result as $id ) {
+			
+		//ez a statemenet számolná meg, hogy hány tag kapcsolódik az adott posthoz
+		$count_tag = $connect->prepare('SELECT COUNT(post_id) FROM posttotag WHERE post_id=?');
+		$count_tag->execute(array($id['id']));
+		$number = $count_tag->fetchColumn();//valahogy el kéne íréni hogy "$result['id']" -re hajtsa végre
+
+		
+		$echo_tags = $connect->prepare('SELECT tag.tag_name, posttotag.tag_id FROM tag INNER JOIN posttotag ON tag.id=posttotag.tag_id WHERE post_id=?');
+		$echo_tags->execute(array($id['id']));
+		$tag_list = $echo_tags->fetchAll(); 
+		
+		
+		
+		echo '<a href="blog.php?id='.$id['id'].'">', $id['id'], '</a>';
+		echo $id['post'];  
+		echo '(', $number, ')';    
+		
+	   
+		
+		foreach($tag_list as $name)
+		{   
+			echo '<a href="tag_post.php?tag='.$name['tag_id'].'">', $name['tag_name'], '</a>', ', ';
+		}
+		
+		echo '<br />';   
+		}
+	}
 
 	
 }
